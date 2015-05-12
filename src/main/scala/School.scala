d38f3200cb3ad2522e03c3fc3b14a64223b3700d
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-05-12 19:42:15 -0400

    clean

--- src/main/scala/School.scala
+++ src/main/scala/School.scala
@@ -1,14 +1,12 @@
 class School {
-  var database = Map[Int, List[String]]()
+  var db = Map[Int, List[String]]()
 
   def sorted = Map()
 
   def grade(i: Int) = Seq()
 
   def add(s: String, i: Int) = {
-    database = database.updated(i, List(s))
+    db = db.updated(i, List(s))
   }
-
-  def db = database
 
 }
